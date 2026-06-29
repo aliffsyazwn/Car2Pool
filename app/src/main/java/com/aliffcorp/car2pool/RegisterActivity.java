2f4f116addc1dcce2ae687b5e4f3cb89591f6195
Faizalsaad445 <2024757571@student.uitm.edu.my>
2026-06-29 10:59:40 +0800

    update button in register.xml

--- app/src/main/java/com/aliffcorp/car2pool/RegisterActivity.java
+++ app/src/main/java/com/aliffcorp/car2pool/RegisterActivity.java
@@ -1,13 +1,26 @@
 package com.aliffcorp.car2pool;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextView txtLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtLogin = findViewById(R.id.txtLogin);
+
+        txtLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
