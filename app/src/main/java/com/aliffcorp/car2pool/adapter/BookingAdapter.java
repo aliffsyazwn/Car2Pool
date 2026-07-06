e117a45cb70f7c2911dc3e0ff60f420c1734b9da
danishimran <2024741247@student.uitm.edu.my>
2026-07-06 21:45:41 +0800

    booking adapter

---
+++ app/src/main/java/com/aliffcorp/car2pool/adapter/BookingAdapter.java
@@ -0,0 +1,50 @@
+package com.aliffcorp.car2pool.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aliffcorp.car2pool.R;
+import com.aliffcorp.car2pool.model.Ride;
+
+import java.util.List;
+
+public class BookingAdapter extends RecyclerView.Adapter<BookingAdapter.BookingViewHolder> {
+
+    private List<Ride> rideList;
+
+    public BookingAdapter(List<Ride> rideList) {
+        this.rideList = rideList;
+    }
+
+    public static class BookingViewHolder extends RecyclerView.ViewHolder {
+        TextView rideInfoText;
+
+        public BookingViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public BookingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_booking, parent, false);
+        return new BookingViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookingViewHolder holder, int position) {
+        Ride currentRide = rideList.get(position);
+
+
+        // holder.rideInfoText.setText("Pemandu: Hafiz - " + currentRide.getDestination());
+    }
+
+    @Override
+    public int getItemCount() {
+        return rideList == null ? 0 : rideList.size();
+    }
+}
