45f0c406e76e70474ecd6d61eec71cf0fb16cce6
danishimran <2024741247@student.uitm.edu.my>
2026-07-06 21:48:08 +0800

    update BookingList

--- app/src/main/java/com/aliffcorp/car2pool/BookingList.java
+++ app/src/main/java/com/aliffcorp/car2pool/BookingList.java
@@ -1,24 +1,23 @@
 package com.aliffcorp.car2pool;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class BookingList extends AppCompatActivity {
+import com.aliffcorp.car2pool.adapter.BookingAdapter;
+import com.aliffcorp.car2pool.model.Ride;
+import com.aliffcorp.car2pool.remote.ApiUtils;
+import com.aliffcorp.car2pool.remote.RideService;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_booking_list);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
