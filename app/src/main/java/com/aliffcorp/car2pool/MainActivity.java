deb8bfa4f61431c85eefa4621ec8192c23697fc8
danishimran <2024741247@student.uitm.edu.my>
2026-07-08 13:35:45 +0800

    update MainActivity

--- app/src/main/java/com/aliffcorp/car2pool/MainActivity.java
+++ app/src/main/java/com/aliffcorp/car2pool/MainActivity.java
@@ -14,7 +14,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.aliffcorp.car2pool.model.User;
-import com.aliffcorp.car2pool.sharedpref.BookingList;
 import com.aliffcorp.car2pool.sharedpref.SharedPrefManager;
 
 public class MainActivity extends AppCompatActivity {
