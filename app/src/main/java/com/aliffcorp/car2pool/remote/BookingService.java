9eda11e054c98f588e7f670e0303810034011c9d
danishimran <2024741247@student.uitm.edu.my>
2026-07-08 14:02:26 +0800

    update BookingService

---
+++ app/src/main/java/com/aliffcorp/car2pool/remote/BookingService.java
@@ -0,0 +1,20 @@
+package com.aliffcorp.car2pool.remote;
+
+import com.aliffcorp.car2pool.model.Booking;
+import com.aliffcorp.car2pool.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+
+public interface BookingService {
+
+    @GET("bookings")
+    Call<List<Booking>> getBookings(@Header("api-key") String api_key);
+
+    @GET("bookings/{booking_id}")
+    Call<Booking> getBooking(@Header("api-key") String api_key, @Path("booking_id") int bookingId);
+}
